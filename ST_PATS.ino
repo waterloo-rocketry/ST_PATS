772c6b32ee6a132f7c5c7c9f99724ef1e9a222c5
Zachariah Mears <zx100x100@gmail.com>
2020-09-29 18:56:50 -0400

    Revert "Delete all files."
    This reverts commit b707a886bbd1c5207979c834bcf842708338bed1.

---
+++ ST_PATS.ino
@@ -0,0 +1,221 @@
+/*
+ * IMPORTANT: In addition to the required libraries the serial RX buffer needs to be set to 256 bytes instead of 64
+ * you should find this number in HardwareSerial.h at somewhere like this:
+ * ...\ArduinoData\packages\STM32\hardware\stm32\1.4.0\cores\arduino
+ */
+
+#include <Wire.h>
+#include "display.h"
+#include "BigRedBee.h"
+#include "RocketCAN.h"
+#include "compass.h"
+#include "navigation.h"
+#include "EEPROM_storage.h"
+#include <Adafruit_GPS.h>
+
+#define TIME_BETWEEN_UPDATES 1000 // in ms
+#define TIME_BETWEEN_SCREEN_RESET 60000 // in ms
+
+#define RED_BUTTON PB11
+#define GREEN_BUTTON PB10
+#define BLUE_BUTTON PE15
+
+#define RED_LED PE0
+#define GREEN_LED PB0
+#define BLUE_LED PA0
+
+static enum {
+  BIG_RED_BEE,
+  ROCKET_CAN,
+  LOCAL
+}nav_GPS;
+
+unsigned long time_of_last_update = 0;
+unsigned long time_of_last_screen_reset = 0;
+
+EEPROM_data EEPROM_nav_data;
+
+
+// Create objects for all our sensors:
+
+Display display;
+
+Compass compass;
+
+HardwareSerial Serial5(PD2, PC12);
+BigRedBee brb_GPS(&Serial5);
+
+HardwareSerial Serial4(PC11, PC10);
+RocketCAN CAN_GPS(&Serial4);
+
+HardwareSerial Serial3(PC5, PC4);
+Adafruit_GPS local_GPS(&Serial3);
+
+/* timer stuff */
+static stimer_t Timer_Handler;
+
+/*
+ * Function for parsing all the gps data once per millisecond.
+ * unused is a pointer to stimer_t or something, whatever it is we just have it to make function definitions happy
+ */
+static void read_gps_data(stimer_t *unused){
+  char c = local_GPS.read();
+  CAN_GPS.read();
+}
+
+
+void setup(void) 
+{
+  EEPROM_nav_data.nav = false;
+  EEPROM_nav_data.latitude = 0;
+  EEPROM_nav_data.longitude = 0;
+  display.power_up();
+  display.begin();
+  display.set_background();
+  
+  Serial.begin(9600);
+  Serial.println("ST PATS"); Serial.println("");
+  
+  /* lets set up our sensors */
+  local_GPS.begin(9600);
+  local_GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);  // turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  local_GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+
+  brb_GPS.begin(9600);
+  CAN_GPS.begin(9600);
+  compass.begin();
+
+  /* lets set up our interupt timer for reading all the UART data */
+  Timer_Handler.timer = TIM4;
+  /* Timer set to 1ms, */
+  TimerHandleInit(&Timer_Handler, 1, (uint32_t)(getTimerClkFreq(TIM4) / 1000));
+  attachIntHandle(&Timer_Handler, read_gps_data);
+
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+
+  pinMode(RED_BUTTON, INPUT_PULLUP);
+  pinMode(GREEN_BUTTON, INPUT_PULLUP);
+  pinMode(BLUE_BUTTON, INPUT_PULLUP);
+
+}
+
+void loop(void) 
+{
+  if(millis() - time_of_last_update > TIME_BETWEEN_UPDATES){
+    time_of_last_update = millis();
+
+    for(int index = 0; index < 20; index++){
+      brb_GPS.parse_data();
+    }
+    
+    compass.read();
+    //display.draw_arrow(compass.get_heading());
+
+  if(EEPROM_nav_data.nav){
+    get_GPS_location(EEPROM_nav_data.latitude, EEPROM_nav_data.longitude, nav_GPS);
+    display.draw_arrow(target_heading(local_GPS.latitudeDegrees, local_GPS.longitudeDegrees,
+    EEPROM_nav_data.latitude, EEPROM_nav_data.longitude));
+  }
+  else {
+    if(nav_GPS == BIG_RED_BEE){
+      display.draw_arrow(target_heading(local_GPS.latitudeDegrees, local_GPS.longitudeDegrees,
+      brb_GPS.latitude, brb_GPS.longitude));
+    }
+    else if(nav_GPS == ROCKET_CAN){
+      display.draw_arrow(target_heading(local_GPS.latitudeDegrees, local_GPS.longitudeDegrees,
+      CAN_GPS.latitude, CAN_GPS.longitude));
+    }
+    else {
+      display.draw_arrow(target_heading(0, 0, 1, 0));
+    }
+  }
+    display.write_GPS("BRBee:", brb_GPS.time_since_last_msg()/1000.0, brb_GPS.latitude, brb_GPS.longitude, nav_GPS == BIG_RED_BEE);
+    display.write_GPS("R_CAN:", CAN_GPS.time_since_last_msg()/1000.0, CAN_GPS.latitude, CAN_GPS.longitude, nav_GPS == ROCKET_CAN);
+
+  if(EEPROM_nav_data.nav){
+      display.write_GPS("Local:", 
+      distance_to_target(local_GPS.latitudeDegrees, local_GPS.longitudeDegrees, EEPROM_nav_data.latitude, EEPROM_nav_data.longitude), 
+      local_GPS.latitudeDegrees, local_GPS.longitudeDegrees, nav_GPS == LOCAL);
+  }
+
+  else {
+    if(nav_GPS == BIG_RED_BEE){
+      display.write_GPS("Local:", distance_to_target(local_GPS.latitudeDegrees, local_GPS.longitudeDegrees,
+      brb_GPS.latitude, brb_GPS.longitude), local_GPS.latitudeDegrees, local_GPS.longitudeDegrees, nav_GPS == LOCAL);
+    }
+    else if(nav_GPS == ROCKET_CAN){
+      display.write_GPS("Local:", distance_to_target(local_GPS.latitudeDegrees, local_GPS.longitudeDegrees,
+      CAN_GPS.latitude, CAN_GPS.longitude), local_GPS.latitudeDegrees, local_GPS.longitudeDegrees, nav_GPS == LOCAL);
+    }
+    else {
+      display.write_GPS("Local:", distance_to_target(0, 0, 0, 0), local_GPS.latitudeDegrees, local_GPS.longitudeDegrees, nav_GPS == LOCAL);
+    }
+  }
+
+    if(EEPROM_nav_data.nav){
+      display.write_GPS("EEPROM:", nav_GPS, EEPROM_nav_data.latitude, EEPROM_nav_data.longitude, true);
+    }
+    else{
+      display.clear_GPS();
+    }
+
+    display.write_local_data(local_GPS.hour, local_GPS.minute, local_GPS.seconds, local_GPS.satellites);
+
+    Serial.print("Compass Heading: ");
+    Serial.println(compass.get_heading());
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (local_GPS.newNMEAreceived()) {
+    local_GPS.parse(local_GPS.lastNMEA());
+  }
+  if(digitalRead(RED_BUTTON) == HIGH){
+    digitalWrite(RED_LED, HIGH);
+  }
+  else{
+    digitalWrite(RED_LED, LOW);
+    if(nav_GPS == LOCAL){
+      nav_GPS = BIG_RED_BEE;
+    }
+    else if(nav_GPS == BIG_RED_BEE){
+      nav_GPS = ROCKET_CAN;
+    }
+    else {
+      nav_GPS = LOCAL;
+    }
+  }
+  if(digitalRead(GREEN_BUTTON) == HIGH){
+    digitalWrite(GREEN_LED, HIGH);
+  }
+  else{
+    digitalWrite(GREEN_LED, LOW);
+    if(nav_GPS == LOCAL){
+      save_GPS_location(local_GPS.latitudeDegrees, local_GPS.longitudeDegrees, LOCAL);
+    }
+    else if(nav_GPS == BIG_RED_BEE){
+      save_GPS_location(brb_GPS.latitude, brb_GPS.longitude, BIG_RED_BEE);
+    }
+    else {
+      save_GPS_location(CAN_GPS.latitude, CAN_GPS.longitude, ROCKET_CAN);
+    }
+  }
+  if(digitalRead(BLUE_BUTTON) == HIGH){
+    digitalWrite(BLUE_LED, HIGH);
+  }
+  else{
+    digitalWrite(BLUE_LED, LOW);
+    EEPROM_nav_data.nav = !EEPROM_nav_data.nav;
+  }
+  }
+  
+  if(millis() - time_of_last_screen_reset > TIME_BETWEEN_SCREEN_RESET){
+    time_of_last_screen_reset = millis();
+    display.power_down();
+    delay(50);
+    display.power_up();
+    display.begin();
+    display.set_background();
+  }
+
+}
---
+++ display.cpp
@@ -0,0 +1,115 @@
+#include "display.h"
+
+Display::Display(){
+  
+}
+
+void Display::power_up(){
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, LOW);
+}
+
+void Display::power_down(){
+  pinMode(POWER_PIN, OUTPUT);
+  digitalWrite(POWER_PIN, HIGH);
+}
+
+void Display::begin(){
+  tft.begin();
+}
+
+void Display::set_background(){
+  tft.fillScreen(ILI9341_WHITE);
+  tft.setRotation(1);
+  tft.setCursor(200, 165);
+  tft.setTextColor(ILI9341_BLUE);  tft.setTextSize(2);
+  tft.print("Time: ");
+  tft.setCursor(200, 205);
+  tft.print("Sats:");
+}
+
+void Display::write_GPS(String GPS_name, int time, double latitude, double longditude, bool state){
+  if (state)
+    tft.setTextColor(ILI9341_MAGENTA); 
+  else
+    tft.setTextColor(ILI9341_BLUE);
+  tft.setTextSize(2);
+  tft.print(GPS_name);
+  tft.setTextColor(ILI9341_RED, ILI9341_WHITE);
+  tft.print(time);
+  tft.println(" ");
+  //tft.print("Lat:");
+  tft.println(latitude, 5);
+  //tft.print("Lon:");
+  tft.println(longditude, 5);
+  tft.println("");
+}
+
+void Display::clear_GPS(){
+  tft.setTextColor(ILI9341_RED, ILI9341_WHITE);
+  tft.println("        ");
+  tft.println("          ");
+  tft.println("          ");
+}
+
+void Display::write_local_data(int hour, int minutes, int seconds, int num_sats){
+  tft.setCursor(200, 185);
+  tft.setTextColor(ILI9341_RED, ILI9341_WHITE);
+  tft.print(hour);
+  tft.print(":");
+  tft.print(minutes);
+  tft.print(":");
+  tft.print(seconds);
+  tft.print(" ");
+  tft.setTextColor(ILI9341_BLUE);
+  tft.setCursor(250, 205);
+  tft.setTextColor(ILI9341_RED, ILI9341_WHITE);
+  tft.print(num_sats);
+  tft.println(" ");
+}
+
+void Display::draw_arrow(int heading){
+
+/* UNTILL THE COMPASS IS WORKING THERE IS NO POINT DRAWING THE ARROW
+    // erase the old triangle
+    static int previous_heading = 0;
+    double x_cord_0 = 0, x_cord_1 = -25, x_cord_2 = 25;
+    double y_cord_0 = NAV_BALL_RADIUS + 8, y_cord_1 = NAV_BALL_RADIUS *-0.75 - 2, y_cord_2 = NAV_BALL_RADIUS *-0.75 - 2;
+    
+    rotate(x_cord_0, y_cord_0, previous_heading*PI/-180);
+    rotate(x_cord_1, y_cord_1, previous_heading*PI/-180);
+    rotate(x_cord_2, y_cord_2, previous_heading*PI/-180);
+    
+    tft.fillTriangle(round(x_cord_0 + NAV_BALL_CENTER_X_CORD), round(y_cord_0 + NAV_BALL_CENTER_Y_CORD),
+    round(x_cord_1 + NAV_BALL_CENTER_X_CORD), round(y_cord_1 + NAV_BALL_CENTER_Y_CORD), 
+    round(x_cord_2 + NAV_BALL_CENTER_X_CORD), round(y_cord_2 + NAV_BALL_CENTER_Y_CORD), ILI9341_WHITE);
+
+    previous_heading = heading;
+
+    // draw the new triangle
+    
+    x_cord_0 = 0, x_cord_1 = -20, x_cord_2 = 20;
+    y_cord_0 = NAV_BALL_RADIUS, y_cord_1 = NAV_BALL_RADIUS *-0.75, y_cord_2 = NAV_BALL_RADIUS *-0.75;
+    
+    rotate(x_cord_0, y_cord_0, heading*PI/-180);
+    rotate(x_cord_1, y_cord_1, heading*PI/-180);
+    rotate(x_cord_2, y_cord_2, heading*PI/-180);
+    
+    tft.fillTriangle(round(x_cord_0 + NAV_BALL_CENTER_X_CORD), round(y_cord_0 + NAV_BALL_CENTER_Y_CORD),
+    round(x_cord_1 + NAV_BALL_CENTER_X_CORD), round(y_cord_1 + NAV_BALL_CENTER_Y_CORD), 
+    round(x_cord_2 + NAV_BALL_CENTER_X_CORD), round(y_cord_2 + NAV_BALL_CENTER_Y_CORD), ILI9341_BLACK);
+
+*/
+    tft.setTextColor(ILI9341_BLACK, ILI9341_WHITE);  
+    tft.setTextSize(5); // 4 when drawing arrow
+    tft.setCursor(160, 50); // (240, 1) when drawing arrow
+    tft.print(heading);
+    tft.print("  ");
+    tft.setCursor(0, 1); // put the cursor back so we can write cordinates
+}
+
+void rotate(double & x_cord, double & y_cord, double theta){
+  double temp_x_cord = x_cord;
+  x_cord = (temp_x_cord*cos(theta)) - (y_cord*sin(theta));
+  y_cord = (temp_x_cord*sin(theta)) + (y_cord*cos(theta));
+}
