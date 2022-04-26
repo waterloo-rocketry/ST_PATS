e66336ff99cd143b66ae39193d046233c90209fd
Rio Liu <rio.liu@r26.me>
2022-04-26 15:44:42 -0400

    add create_gps_message function for reference

--- src/can_radio.c
+++ src/can_radio.c
@@ -35,6 +35,55 @@ uint8_t base64_to_binary(char base64)
     return 255;
 }
 
+bool create_gps_message(uint8_t latitude_deg,
+                        uint8_t latitude_min,
+                        uint16_t latitude_dmin,
+                        uint8_t latitude_dir,
+                        uint8_t longitude_deg,
+                        uint8_t longitude_min,
+                        uint16_t longitude_dmin,
+                        uint8_t longitude_dir,
+                        char *str)
+{
+    if (str == NULL ||
+        (latitude_dir != 'N' && latitude_dir != 'S') ||
+        (longitude_dir != 'E' && longitude_dir != 'W')) {
+        return false;
+    }
+
+    str[0] = GPS_MSG_HEADER;
+
+    str[1] = binary_to_base64((latitude_deg >> 2) & 0x3f);
+    str[2] = binary_to_base64(((latitude_deg << 4) & 0x30) |
+                              ((latitude_min >> 4) & 0xf));
+
+    str[3] = binary_to_base64(((latitude_min << 2) & 0x3c) |
+                              ((latitude_dmin >> 14) & 0x3));
+
+    str[4] = binary_to_base64(latitude_dmin >> 8 & 0x3f);
+    str[5] = binary_to_base64((latitude_dmin >> 2) & 0x3f);
+    str[6] = binary_to_base64(((latitude_dmin << 4) & 0x30) |
+                              ((longitude_deg >> 4) & 0xf));
+
+    str[7] = binary_to_base64(((longitude_deg << 2) & 0x3c) |
+                              ((longitude_min >> 6) & 0x3));
+
+    str[8] = binary_to_base64(longitude_min & 0x3f);
+    str[9] = binary_to_base64((longitude_dmin >> 10) & 0x3f);
+    str[10] = binary_to_base64((longitude_dmin >> 4) & 0x3f);
+
+    str[11] = binary_to_base64(((longitude_dmin << 2) & 0x3c) |
+                               (latitude_dir == 'N' ? 0x2 : 0) |
+                               (longitude_dir == 'E' ? 0x1 : 0));
+
+    // calculate checksum
+    str[GPS_MSG_LEN - 1] = '\0';
+    char cs = checksum(str);
+    str[GPS_MSG_LEN - 1] = cs;
+
+    return true;
+}
+
 bool expand_gps_message(uint8_t *latitude_deg,
                         uint8_t *latitude_min,
                         uint16_t *latitude_dmin,
